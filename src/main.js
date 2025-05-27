13c19e237384cdfa729760b2f9d015de4825bdc8
宫仅 <8281538+HHandHsome@user.noreply.gitee.com>
2025-05-28 03:25:42 +0800

    fix: main.js 修复了点击 short 无法自动跳转到长视频的 bug

--- src/main.js
+++ src/main.js
@@ -1,10 +1,79 @@
 import {CornMenuManager} from "./lib/CornMenuManager.js";
+import {elmGetter} from "./lib/elmGetter.js";
+import "./style/main.css"
 
 async function main() {
     createMenu()
 }
 
 main();
+
+function createMenu() {
+    CornMenuManager.addAndCreate([
+        {
+            default: true,
+            callback(state, isInit){
+                if (!isInit) {
+                    // 刷新页面
+                    location.reload();
+                }
+
+                if (!state) {
+                    return;
+                }
+
+                async function processState() {
+                    await elmGetter.each(".shortsLockupViewModelHostEndpoint", (el) => {
+                        console.log(el);
+
+                        el.href = convertShortsToVideoLink(el.href);
+                    })
+
+                    // 这里无法 hook history 路由
+                    // 因为 shadow DOM 也无法添加 eventListener
+                    // 只能用此招化解, 狸猫换太子
+                    await elmGetter.each("ytm-shorts-lockup-view-model-v2", (el) => {
+                        let mask = document.createElement("div")
+
+                        mask.className = "mask"
+
+                        el.appendChild(mask);
+
+                        mask.addEventListener("click", () => {
+                            const aEl = el.querySelector(`a`)
+
+                            location.href = aEl.href;
+                        })
+                    })
+                }
+
+                jmpToVideo();
+
+                processState();
+            },
+            on : {
+                name : "自动跳转状态: 开启✅ (点我关闭)",
+            },
+            off : {
+                name : "自动跳转状态: 关闭❎ (点我开启)",
+            }
+        },
+        {
+            name: "跳转到 Video",
+            callback() {
+                jmpToVideo();
+            }
+        }
+    ]);
+}
+
+function onShortElClick() {
+    setTimeout(() => {
+        console.log("路径发生了变化");
+
+        jmpToVideo();
+    }, 100);
+}
 
 /*
     input: https://www.youtube.com/shorts/6yjwN-KVIrk
@@ -26,30 +95,6 @@ function jmpToVideo() {
     }
 }
 
-function createMenu() {
-    CornMenuManager.addAndCreate([
-        {
-            default: true,
-            callback(state, isInit){
-                if (state) {
-                    jmpToVideo();
-                }
-            },
-            on : {
-                name : "自动跳转状态: 开启✅ (点我关闭)",
-            },
-            off : {
-                name : "自动跳转状态: 关闭❎ (点我开启)",
-            }
-        },
-        {
-            name: "跳转到 Video",
-            callback() {
-                jmpToVideo();
-            }
-        }
-    ]);
-}
 
 // TODO: 关闭 enable
 function print(msg, {enable = true, tag = "油猴脚本: "} = {}) {
